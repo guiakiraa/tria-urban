0f90b69147925a38405f6321bfd52ffd2ddc51fa
guiakiraa <guilhermeakira0810@gmail.com>
2025-06-02 11:19:08 -0300

    feat: Swagger Config

---
+++ src/main/java/br/com/fiap/triaurban/swagger/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package br.com.fiap.triaurban.swagger;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI configurarSwagger() {
+        return new OpenAPI().info(new Info()
+                .title("Projeto de Gestão de Motos da empresa Mottu")
+                .description("Este projeto oferece uma implementação que possibilita "
+                        + "a gestão de motos dentro dos pátios da empresa Mottu")
+                .summary("A fim de resolver o problema de perda de motos dentro dos pátios da Mottu, "
+                        + "desenvolvemos esse projeto com o intuíto de não deixar as motos ficarem em setores "
+                        + "em que elas não deveriam estar"
+                )
+                .version("v1.0.0")
+                .license(new License()
+                        .url("www.fiap.com.br")
+                        .name("Licença - Projeto de Gestão de Motos - v1.0.0"))
+                .termsOfService("Termos de Serviço")
+        );
+    }
+}
